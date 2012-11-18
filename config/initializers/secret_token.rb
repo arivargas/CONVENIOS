a91e1aff6bc44f86a50c99373d9fafba267f46e9
Ariela Maria <arivargas17@gmail.com>
2012-11-17 19:14:09 -0800

    Created Convenios app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Convenios::Application.config.secret_token = '8407b8c5390e56863f094f9fde5b11196ac85fea63c3720357d3e0fe37a14738d4eef79d59a653ba3b87dbf4fc545f99311c7d374e49a5a8ca4f29dc5413cf6a'
